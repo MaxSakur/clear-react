d5f7477b9e977bcb32a0933f6431d3ac4c2268dc
Bogdan <bsshul@gmail.com>
2022-09-21 18:32:35 +0300

    modalOff

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import { Modal } from './components/Modal/Modal';
 function App() {
   return (
     <>
-      <Modal />
+      {/* <Modal /> */}
       <div className="App">
         <MainContainer>
           <Menu />
