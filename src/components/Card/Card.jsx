0fdc71aa9d6e7ca8d19354efd944defeb64f2c71
Bogdan <bsshul@gmail.com>
2022-09-21 22:35:44 +0300

    3d rotate

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -3,11 +3,19 @@ import styles from './Card.module.scss'
 
 export const Card = () => {
   return (
-    <>
+    <div className={styles['wrapper']}>
+      <div className={styles['card-corner']}></div>
       <div className={styles['card']}>
-        <div className={styles['card-corner']}></div>
-        <input type="text" placeHolder='Add new word...' />
+        {/* <div className={styles['card-corner']}></div> */}
+        <input
+          type="text"
+          className={styles['card-input']}
+          placeHolder='Add new word...' />
       </div>
-    </>
+      <div className={styles['card-translate']}>
+        {/* <div className={styles['card-corner']}></div> */}
+        <input type="text" className={styles['card-translate-input']} placeHolder='Add translate...' />
+      </div>
+    </div >
   )
 }
