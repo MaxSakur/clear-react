166e8794e0f577158599514c0743ca8de7600cc9
Max Kaliakin (PC) <sakurwork@gmail.com>
2022-09-25 16:29:43 +0300

    test

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
-import MainContainer from './components/MainContainer';
+import MainContainer from "./components/MainContainer";
 
-import Menu from './components/Menu';
-import { MainPage } from './components/MainPage/MainPage';
-import { Modal } from './components/Modal/Modal';
-import { useWindowSize } from './hooks'
+import Menu from "./components/Menu";
+import { MainPage } from "./components/MainPage/MainPage";
+import { Modal } from "./components/Modal/Modal";
+import { useWindowSize } from "./hooks";
 
 function App() {
-  const [width, height] = useWindowSize()
+  const [width, height] = useWindowSize();
   return (
     <>
-      <div>width: {width} height: {height}</div>
+      <div>
+        width: {width} height: {height}
+      </div>
 
-      {width < 1100 && width > 650 ? <Modal /> : null}
+      {width < 1200 && width > 650 ? <Modal /> : null}
 
       <div className="App">
         <MainContainer>
