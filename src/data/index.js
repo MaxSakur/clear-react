9954fec8b2db1b8b49bbfb515135956511087e79
Bogdan <bsshul@gmail.com>
2022-10-02 11:34:34 +0300

    Add translate key

--- src/data/index.js
+++ src/data/index.js
@@ -3,40 +3,48 @@ export const CATEGORIES = [
     id: 0,
     name: "Body",
     data: ["bag", "head", "shoulder", "arm", "hand", "elbow"],
+    translate: ["", "", "", "", "", ""],
   },
   {
     id: 1,
     name: "Colors",
     data: ["aquamarine", "beige", "snowy", "white", "turquoise", "pale"],
+    translate: ["", "", "", "", "", ""],
   },
   {
     id: 2,
     name: "City",
     data: ["town", "city", "building", "skyscraper", "palace", "bank"],
+    translate: ["", "", "", "", "", ""],
   },
   {
     id: 3,
     name: "Food",
     data: ["salate", "ham", "sausage", "meat", "egg", "pizza"],
+    translate: ["", "", "", "", "", ""],
   },
   {
     id: 4,
     name: "Animals",
     data: ["bear", "fox", "rabbit", "wolf", "pig", "deer"],
+    translate: ["", "", "", "", "", ""],
   },
   {
     id: 5,
     name: "Clothes",
     data: ["blouse", "skirt", "dress", "shirt", "trousers", "socks"],
+    translate: ["", "", "", "", "", ""],
   },
   {
     id: 6,
     name: "Kitchen",
     data: ["cabinet", "microwave", "canister", "stove", "burner", "oven"],
+    translate: ["", "", "", "", "", ""],
   },
   {
     id: 7,
     name: "Time",
     data: ["past", "future", "present", "age", "millenium", "century"],
+    translate: ["", "", "", "", "", ""],
   },
 ];
